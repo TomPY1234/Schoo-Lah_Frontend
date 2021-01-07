33403a26a4da3fcb32a7f191c3ef0219a98a4f81
TeamExia <nicholaspng-0202@hotmail.com>
2021-01-07 11:02:35 +0800

    Create Bookstore for Student

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 //import 'package:schoolah_mobile_app/mainScreens/splashscreen.dart';
-import 'package:schoolah_mobile_app/studentScreens/studentprofile.dart';
+import 'package:schoolah_mobile_app/studentScreens/bookstore.dart';
 
 void main() => runApp(
   MaterialApp(
     title: 'Schoo-Lah',
     debugShowCheckedModeBanner: false,
     theme: ThemeData(primarySwatch: Colors.orange),
-    home: StudentProfileScreen(),
+    home: BookstorePageScreen(),
   ),
 );
---
+++ lib/studentScreens/bookstore.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class BookstorePageScreen extends StatefulWidget 
+{
+  @override 
+  _BookstorePageState createState() => _BookstorePageState();
+}
+
+class _BookstorePageState extends State <BookstorePageScreen>
+{
+  int _selectedIndex = 1;
+
+  void _onItemTapped(int index)
+  {
+    if (index == 0)
+    { setState(() { _selectedIndex = index; }); }
+    else if (index == 1)
+    { setState(() { _selectedIndex = index; });}
+    else 
+    { setState(() { _selectedIndex = index; });}
+  }
+
+  @override 
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Icon(Icons.settings, size: 40.0),
+        title: Text('BOOKSTORE'),
+        centerTitle: true,
+        actions: <Widget> [
+          Icon(Icons.calendar_today_rounded, size: 35.0),
+          SizedBox(width: 12.0)
+        ],
+      ),
+
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.orange[200],
+              Colors.orange[50],
+              Colors.orange[200],
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: ListView.separated(
+          itemCount: 4,
+          separatorBuilder: (context, index) => Divider(color: Colors.black, thickness: 5.0,),
+          itemBuilder: (context, index) => ListTile(
+            tileColor: Colors.deepOrange[700],
+            leading: Image.asset('assets/book_one.jpeg'),
+            title: Text('Bahasa Melayu Tatabahasa Tahun 1', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 17.0)),
+            subtitle: Text('RM 20.00', style: TextStyle(color: Colors.white)),
+            onTap: () {},
+          ),
+        ),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner_rounded),
+            label: 'QRScan',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.grey[500],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
