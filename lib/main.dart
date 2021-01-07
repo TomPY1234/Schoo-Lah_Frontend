aa3c2971f6a22bf662d37f34a3c9e8716ec1aa89
TeamExia <nicholaspng-0202@hotmail.com>
2021-01-07 13:49:57 +0800

    Update home of runApp() in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 //import 'package:schoolah_mobile_app/mainScreens/splashscreen.dart';
-import 'package:schoolah_mobile_app/studentScreens/tuitionfee.dart';
+import 'package:schoolah_mobile_app/mainScreens/splashscreen.dart';
 
 void main() => runApp(
   MaterialApp(
     title: 'Schoo-Lah',
     debugShowCheckedModeBanner: false,
     theme: ThemeData(primarySwatch: Colors.orange),
-    home: TuitionFeeScreen(),
+    home: SchoolahSplashScreen(),
   ),
 );
