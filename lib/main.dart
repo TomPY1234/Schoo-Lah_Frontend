fd938dfffa859b85a5d32216f2ffee03a9aae518
TeamExia <nicholaspng-0202@hotmail.com>
2021-01-07 11:01:03 +0800

    Create Student Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 //import 'package:schoolah_mobile_app/mainScreens/splashscreen.dart';
-import 'package:schoolah_mobile_app/studentScreens/tasklist.dart';
+import 'package:schoolah_mobile_app/studentScreens/studentprofile.dart';
 
 void main() => runApp(
   MaterialApp(
     title: 'Schoo-Lah',
     debugShowCheckedModeBanner: false,
     theme: ThemeData(primarySwatch: Colors.orange),
-    home: StudentTaskListScreen(),
+    home: StudentProfileScreen(),
   ),
 );
