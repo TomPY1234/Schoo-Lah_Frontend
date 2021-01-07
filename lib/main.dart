44575a7d8604c5fd9de5be9257d745a853c8e997
TeamExia <nicholaspng-0202@hotmail.com>
2021-01-07 10:41:23 +0800

    Create Student HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'package:schoolah_mobile_app/mainScreens/splashscreen.dart';
+//import 'package:schoolah_mobile_app/mainScreens/splashscreen.dart';
+import 'package:schoolah_mobile_app/studentScreens/studenthome.dart';
 
 void main() => runApp(
   MaterialApp(
     title: 'Schoo-Lah',
     debugShowCheckedModeBanner: false,
     theme: ThemeData(primarySwatch: Colors.orange),
-    home: SchoolahSplashScreen(),
+    home: StudentHomePageScreen(),
   ),
 );
