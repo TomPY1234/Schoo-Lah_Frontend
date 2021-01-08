9afb399f74a3ec64ffacf808dbb4282f8f1af917
AqilahFadzil <nurulaqilahfadzil@gmail.com>
2021-01-08 18:34:02 +0800

    Student screen navigate

---
+++ lib/models/data.dart
@@ -0,0 +1,16 @@
+import 'user.dart';
+
+final globalUserList = <User>[
+  User(
+      username: 'ahmadaiman',
+      password: 'ahmad123',
+      name: 'Ahmad Aiman',
+      type: 'student',
+      year: 5),
+  User(
+      username: 'nurain',
+      password: 'ain123',
+      name: 'Nur Ain',
+      type: 'teacher',
+      year: 0),
+];
---
+++ lib/models/user.dart
@@ -0,0 +1,9 @@
+class User {
+  String username;
+  String password;
+  String name;
+  String type;
+  int year;
+
+  User({this.username, this.password, this.name, this.type, this.year});
+}
