1570bb7bc3c51202412c375d225a2962325253c1
Fikrimail <fikrismail99@gmail.com>
2021-01-15 17:23:03 +0800

    Merge Fee and Student list function

--- lib/studentScreens/tuitionfee.dart
+++ lib/studentScreens/tuitionfee.dart
@@ -145,23 +145,23 @@ Card topArea() => Card(
 /*Container displayFeeList(List<Fee> fees
     {Color oddColor = Colors.white}) {
   return Container(
-  //  decoration: BoxDecoration(color: oddColor),
+    decoration: BoxDecoration(color: oddColor),
     padding: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 5.0, right: 5.0),
     child: Column(
       children: <Widget>[
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-         //   Text(fees[].monthFee, style: TextStyle(fontSize: 16.0)),
-         //   Text(date, style: TextStyle(fontSize: 16.0)),
+            Text(fees[].monthFee, style: TextStyle(fontSize: 16.0)),
+            Text(date, style: TextStyle(fontSize: 16.0)),
           ],
         ),
         SizedBox(height: 10.0),
         Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-          //    Text(feeStatus,
-          //        style: TextStyle(color: Colors.grey, fontSize: 14.0)),
+              Text(feeStatus,
+                  style: TextStyle(color: Colors.grey, fontSize: 14.0)),
               Container(
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
